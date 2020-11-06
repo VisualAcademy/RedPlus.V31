cfd7a7f85c5724e7d48dffb15edc88aaeb129f53
RedPlus <redplus@redplus.net>
2020-11-06 18:21:34 +0900

    componentDidMount, state, navigateTo

--- RedPlus/ClientApp/src/components/Entries/EntryCreate.jsx
+++ RedPlus/ClientApp/src/components/Entries/EntryCreate.jsx
@@ -3,11 +3,35 @@
 export class EntryCreate extends Component {
     constructor(props) {
         super(props);
+
+        // 컴포넌트 상태 데이터 
+        this.state = {
+            id: 0, 
+            name: '',
+            title: "",
+            content: "",
+            created: null
+        };
+
+        // 콜백에서 `this`가 작동하려면 아래와 같이 바인딩 해주어야 합니다.
+        // https://ko.reactjs.org/docs/handling-events.html
+        this.navigateToIndex = this.navigateToIndex.bind(this); 
+    }
+
+    // 페이지 로드, OnInitialized()
+    componentDidMount() {
+
     }
 
     render() {
         return (
             <h1>Create</h1>
         );
+    }
+
+    // 리스트 페이지로 이동
+    navigateToIndex() {
+        const { history } = this.props;
+        history.push('/Entries');
     }
 }
--- RedPlus/ClientApp/src/components/Entries/EntryIndex.jsx
+++ RedPlus/ClientApp/src/components/Entries/EntryIndex.jsx
@@ -3,11 +3,31 @@
 export class EntryIndex extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            entries: [],
+            loading: true
+        };
+
+        // 콜백에서 `this`가 작동하려면 아래와 같이 바인딩 해주어야 합니다.
+        // https://ko.reactjs.org/docs/handling-events.html
+        this.navigateToCreate = this.navigateToCreate.bind(this); 
+    }
+
+    // 페이지 로드, OnInitialized()
+    componentDidMount() {
+
     }
 
     render() {
         return (
             <h1>List</h1>    
         );
+    }
+
+    // 글쓰기 페이지로 이동
+    navigateToCreate() {
+        const { history } = this.props;
+        history.push('/Entries/Create'); 
     }
 }
