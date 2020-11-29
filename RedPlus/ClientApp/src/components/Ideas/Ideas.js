779715a49911558c416df9c7a79ca92ff8d888d8
RedPlus <redplus@redplus.net>
2020-11-29 08:53:37 +0000

    React Part 1 Sources

---
+++ RedPlus/ClientApp/src/components/Ideas/Ideas.js
@@ -0,0 +1,20 @@
+﻿import React, { useState } from 'react';
+
+const Ideas = () => {
+    //const [names, setNames] = useState([
+    //    { id: 1, text: '눈사람' },
+    //    { id: 2, text: '얼음' },
+    //    { id: 3, text: '눈' },
+    //    { id: 4, text: '바람' }
+    //]);
+
+    //const [inputText, setInputText] = useState('');
+    //const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+
+    //const namesList = names.map(name => <li key={name.id}>{name.text}</li>);
+    //return <ul>{namesList}</ul>;
+    return <h1>Test</h1>;
+};
+
+export default Ideas;
+
